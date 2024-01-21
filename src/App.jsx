ca2f42e5370e4ae51bc2e52f91d188d6f4711943
Khalid Kaiser <romman.kaiser@gmail.com>
2024-01-21 23:21:46 +0600

    setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hamburger from "./components/Hamburger";
+import About from "./pages/About";
+import Home from "./pages/Home"
+
+function App() {
+  return (
+    <div className="">
+      <Hamburger />
+      <Home />
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hamburger.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function Hamburger() {
+    const [open, setOpen] = useState(false);
+
+  return (
+    <div></div>
+  );
+}
+
+export default Hamburger;
